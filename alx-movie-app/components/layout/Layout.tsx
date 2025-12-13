cd194b37744fc7b89abd5f61c84777f916de4bea
Shema Manasseh <shemamanase992@gmail.com>
2025-12-13 20:12:48 +0300

    feat: set up Header, Footer, Layout and landing page
    - Added Header component with app title
    - Added Footer component with copyright
    - Created Layout wrapper with Header, Footer, and main content
    - Implemented Landing Page using Layout

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,4 +1,15 @@
-const Layoout = () => {
-    return <div>Layout</div>;
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  )
 }
-export default Layoout;
+
+export default Layout;
